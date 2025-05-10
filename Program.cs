4f68e51e26b7f84d5112c498a6d94cf9a81e744d
SauloRodrigues20 <130412074+SauloRodrigues20@users.noreply.github.com>
2025-05-10 18:40:24 +0000

    Azure

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Azure;
+using Azure.AI.OpenAi;
+using Microsoft.Extensions.Configuration;
+using OpenAi.Chat;
+using System.Text;
+
+namespace ConsoleApp.AzureAi{
+    public class TemperatureSettings{
+        public double Temperature {get; set;} = 0.7;
+    }
+
+    public class Program{
+        private static string AzureOpenAiEndpoint = "";
+        private static string AzureOpenAiKey = "";
+        private static string DeploymentName = "";
+    }
+}
